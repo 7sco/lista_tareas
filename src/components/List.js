45393f957dbe58e7986a80ccde415e99bccb26c4
7sco <ISCO@Franciscos-MacBook-Pro.local>
2017-06-09 17:04:38 -0400

    Base for react

---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class List extends Component{
+
+render(){
+	return(
+		<div className="container">
+			<div >HELLOO</div>
+			<input type="text" />
+			<input type="submit" />
+
+		</div>
+
+		
+		);
+
+}
+}
+
+export default List;
